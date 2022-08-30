a139e5eee3cb258bdace622401197a2a5361cdfe
rasmus <c19rwr@cs.umu.se>
2022-08-30 14:30:49 +0200

    File structure

---
+++ src/Parser.java
@@ -0,0 +1,13 @@
+public class Parser {
+
+    public boolean ParseNaive(String s) {
+        return true;
+    }
+    public boolean ParseBU(String s) {
+        return true;
+    }
+    public boolean parseTD(String s) {
+        return true;
+    }
+}
+
