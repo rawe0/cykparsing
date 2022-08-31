6c00b0328abfc3629452838e1842084eb47afccb
Rasmus Welander <c19rwr@cs.umu.se>
2022-08-31 23:35:59 +0200

    kommit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            File myObj = new File(args[0]);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
